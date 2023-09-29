bd458b23067a15e4e7d39976deb649c933cb8d9b
michaljones <micheal.jones2010@yahoo.com>
2023-09-28 22:16:37 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+
+    let thankyoumessage=[];
+
+    for (let i = 0; i < names.length; i++){
+const message= `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+
+thankyoumessage.push(message)}
+return thankyoumessage;
+
+
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(number){
+
+    for (let i = number; i>=0; i--){
+
+        console.log(i)
+    }
+}
+ countDown(5);
